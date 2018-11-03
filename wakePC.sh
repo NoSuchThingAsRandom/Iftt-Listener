508aaae239aafcf86a799fbf3e0ed0a8c6be97cc
NoSuchThingAsRandom <samralph76@gmail.com>
2018-11-03 16:41:03 +0000

    Minor fixes

--- wakePC.sh
+++ wakePC.sh
@@ -1,11 +1,12 @@
+#!/bin/sh
 sam="D8:CB:8A:3C:35:CA"
 pete="48:0F:CF:58:B4:DA"
-if ["$1"="SAM"]; then
+if [ "$1" = "SAM" ]; then
     echo "Booting Sam"
-    etherwake $sam
-elif ["$2"="PETE"]; then
+    sudo etherwake $sam
+elif [ "$1" = "PETE" ]; then
     echo "Booting Pete"
-    etherwake $pete
+    sudo etherwake $pete
 else
     echo "Unknown mac"
 fi
